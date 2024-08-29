3dbd0e18655dc85478f4d55bf9fe04ef01e04f30
sandynicholas <sandynicholas1201@gmail.com>
2024-08-29 10:03:54 +0700

    resume education

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -90,17 +90,22 @@ const education = {
     "5th semester Undergraduate Student of Informatics Bachelor Degree at UPN Veteran Jawa Timur",
   items: [
     {
-      school: "UPN Veteran Jawa Timur",
+      institution: "UPN Veteran Jawa Timur",
       degree: "Informatics Bachelor Degree",
       duration: "2022 - Present",
     },
     {
-      school: "Online Course Platform",
+      institution: "Youtube Programming Chanel",
+      degree: "Full Stack Web Developer Course",
+      duration: "2022 - Present",
+    },
+    {
+      institution: "Online Course Platform",
       degree: "Basic Programming Language Bootcamp",
       duration: "2022 - Present",
     },
     {
-      school: "SMA Negeri 3 Surabaya",
+      institution: "SMA Negeri 3 Surabaya",
       degree: "Natural Sciences High School Diploma",
       duration: "2019 - 2022",
     },
@@ -233,12 +238,41 @@ const Resume = () => {
 
             {/*Education*/}
             <TabsContent value="education" className="w-full">
-              Education
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-autp xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={item.id}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/*dot*/}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/40">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
+
             {/*Skills*/}
             <TabsContent value="skills" className="w-full">
               Skills
             </TabsContent>
+
             {/*About*/}
             <TabsContent value="about" className="w-full">
               About Me
